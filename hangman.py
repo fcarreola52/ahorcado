55602d06e9bea71f33bc47a6d47faea64836bde5
Carlos Francisco Javier Carreola Silva <franciscocarreola@hotmail.com>
2022-10-05 11:30:47 -0500

    ahorcado terminado antes de pasar a list comprenhentios

--- hangman.py
+++ hangman.py
@@ -15,17 +15,74 @@ def letter_list(chosen_word): #funcion que guarda cada letra del archivo en una 
     print(list)
     return(list)
 
-def ahorcado(list,chosen_word): 
-    os.system('clear')
-    print("Bienvenido al juego del ahorcado")
-    print(len(chosen_word)*"_ ")
-    letter=input("ingresa una letra:")
+def guion_list(chosen_word):#funcion que llena de guiones una lista del tamaño de la palabra elegida
+    list2=[]
+    for i in range(len(chosen_word)):
+        list2.append("_ ")
+    return(list2)
+def transform(list): #funcion que transforma listas en palabras
+    palabra=""
+    for i in range(len(list)):
+        palabra=palabra+list[i]
+    return(palabra)
+def replacement(chosen_word): #funcion que cambia los acentos a palabras sin acentos
+    chosen_word=chosen_word.replace("á","a")
+    chosen_word=chosen_word.replace('é','e')
+    chosen_word=chosen_word.replace('í','i')
+    chosen_word=chosen_word.replace('ó','o')
+    chosen_word=chosen_word.replace('ú','a')
+    chosen_word=chosen_word.replace("Á","a")
+    chosen_word=chosen_word.replace('É','e')
+    chosen_word=chosen_word.replace('Í','i')
+    chosen_word=chosen_word.replace('Ó','o')
+    chosen_word=chosen_word.replace('Ú','a')
+    return(chosen_word)
+
+
+
+def ahorcado(list,chosen_word,list2): #esta funcion compara las listas para ver si la lista 2 que se va actualizando ya es igual a la lista original
+    while list != list2:
+        list3=list2
+        os.system('clear')
+        print("Bienvenido al juego del ahorcado")
+        print(transform(list2))
+        letter=input("ingresa una letra: ")
+        letter=replacement(letter)
+        letter=letter.upper()
+        b=0 #contador para ver si entro al ciclo for 
+
+        try:
+            
+            for i in range(len(chosen_word)):
+                if letter==list[i]:
+                    list2[i]=letter
+                    b+=1
+            if b==0:
+                print(f"intentalo de nuevo la letra {letter} no se encuentra en la palabra")
+                mensaje2=input("presiona  enter para continuar")
+            if len(letter) !=1 or letter==" ":
+                raise ValueError
+            
+        except ValueError:
+            print("Rercuerda escribir letras unicamente, numeros y palabras no son validdos, tampoco espacios")
+            mensaje=input("presiona  enter para continuar")
+        
+        
+    if list==list2:
+        os.system('clear')
+        print(f"felicidades has ganado la palabra era {chosen_word}")
+        
+
+
     
 def run():
     words=read()
     chosen_word=words[random.randint(0,len(words)-1)]
+    chosen_word=replacement(chosen_word)
+    chosen_word=chosen_word.upper()    
     list=letter_list(chosen_word)
-    ahorcado(list,chosen_word)
+    list2=guion_list(chosen_word)
+    ahorcado(list,chosen_word,list2)
     
 
     
