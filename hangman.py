d62b9a4e49ea8fa021c659d43861a24525c416af
Carlos Francisco Javier Carreola Silva <franciscocarreola@hotmail.com>
2022-10-05 09:56:19 -0500

    se guardo cada letra de la palabra en una lista

--- hangman.py
+++ hangman.py
@@ -1,15 +1,32 @@
 import random
-def read():
-    palabras=[]
+import os
+def read():#funcion que lee el archivo y lo guarda en una lista, la funcion regresa la lista
+    words=[]
     with open("./data.txt","r", encoding="utf=8") as f: 
         for line in f:
-            palabras.extend((line.split()))
-    #print(palabras)
-    return(palabras)
+            words.extend((line.split()))
+    
+    return(words)
+
+def letter_list(chosen_word): #funcion que guarda cada letra del archivo en una lista, la funcion regresa la lista
+    list=[]
+    for i in chosen_word:
+        list.append(i)
+    print(list)
+    return(list)
+
+def ahorcado(list,chosen_word): 
+    os.system('clear')
+    print("Bienvenido al juego del ahorcado")
+    print(len(chosen_word)*"_ ")
+    letter=input("ingresa una letra:")
+    
 def run():
-    palabras=read()
-    chosen_word=palabras[random.randint(0,len(palabras)-1)]
-    print(chosen_word)
+    words=read()
+    chosen_word=words[random.randint(0,len(words)-1)]
+    list=letter_list(chosen_word)
+    ahorcado(list,chosen_word)
+    
 
     
 if __name__=='__main__':
